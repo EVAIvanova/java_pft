0c4aa78051109a8f2d4e52940e07e41c7577560d
EVAIvanova <skyLena1@ya.ru>
2017-09-25 12:38:42 +0300

    Добавлено несколько классов со вложенными методами

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Myfirstprogram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Myfirstprogram.java
@@ -3,25 +3,22 @@ package ru.stqa.pft.sandbox;
 public class Myfirstprogram {
 
   public static void main(String[] args) {
-    double l = 5;
+
     hello("world");
     hello("user");
     hello("Lena");
-    System.out.println("Площадь квадрата со строной " + l + " =" + area(l));
-    double a=5, b=6;
-    System.out.println("Площадь прямоугольника со сторонами "+ a+" и "+b+ " = "+area (a,b));
+    Square s=new Square (5);
+    System.out.println("Площадь квадрата со строной " + s.l + " =" + s.area());
+    Rectangle r=new Rectangle(4,5);
+    System.out.println("Площадь прямоугольника со сторонами "+ r.a+" и "+r.b+ " = "+r.area ());
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len) {
-    return len * len;
-  }
 
-  public static double area (double a, double b) {
-    return a * b;
-  }
+
+
 }
 
