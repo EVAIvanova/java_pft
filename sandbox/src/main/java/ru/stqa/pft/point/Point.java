f3db970c82b60f51071e69e5da24af94a2ffe1c9
EVAIvanova <skyLena1@ya.ru>
2017-09-26 10:07:30 +0300

    Добавлена функция distance для вычисления расстояния между двумя точками
    Здесь аргументы функции передаются с главного тела программы и имеют тип double.

--- sandbox/src/main/java/ru/stqa/pft/point/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/point/Point.java
@@ -2,6 +2,11 @@ package ru.stqa.pft.point;
 
 public class Point {
   public static void main (String[] args) {
-    System.out.println("Расчет длины отрезка");
+    double x1=2, y1=4;
+    double x2=6, y2=7;
+    System.out.println("Расстояние между двумя точками с координатами т.1 ("+x1+","+y1+") и т.2 ("+x2+","+y2+") = "+ distance (x1,y1,x2,y2));
+  }
+  public static double distance(double x1,double y1, double x2, double y2){
+  return Math.sqrt(Math.pow((y2-y1),2.)+Math.pow((x2-x1),2.));
   }
 }
