e16bfea716434af8035280eba6d1f69612e4655e
EVAIvanova <skyLena1@ya.ru>
2017-09-26 10:26:17 +0300

    Добавлен конструктор в классе point1
    Теперь координты точек могут задаваться непосредственно при описании экземпляров класса

--- sandbox/src/main/java/ru/stqa/pft/point/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/point/Point.java
@@ -2,12 +2,8 @@ package ru.stqa.pft.point;
 
 public class Point {
   public static void main (String[] args) {
-    point1 xy1 = new point1 ();
-    xy1.x=2;
-    xy1.y=4;
-    point1 xy2 = new point1 ();
-    xy2.x=6;
-    xy2.y=7;
+    point1 xy1 = new point1 (2,4);
+    point1 xy2 = new point1 (6,7);
     System.out.println("Расстояние между двумя точками с координатами т.1 ("+xy1.x+","+xy1.y+") и т.2 ("+xy2.x+","+xy2.y+") = "+ distance (xy1,xy2));
   }
   public static double distance(point1 xy1, point1 xy2){
--- sandbox/src/main/java/ru/stqa/pft/point/point1.java
+++ sandbox/src/main/java/ru/stqa/pft/point/point1.java
@@ -1,6 +1,12 @@
 package ru.stqa.pft.point;
 
 public class point1 {
-  double x;
-  double y;
+  public double x;
+  public double y;
+
+  public point1 (double x, double y) {
+    this.x=x;
+    this.y=y;
+  }
+
 }
