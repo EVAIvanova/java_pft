49a6619744546b9e3587739eebd9a510edb55042
EVAIvanova <skyLena1@ya.ru>
2017-12-01 13:16:15 +0200

    подсвечен красным метод toModelMail(g)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appManager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appManager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
 
   public ApplicationManager(String browser) {
@@ -79,6 +80,13 @@ public class ApplicationManager {
 
     }
     return wd;
+  }
+
+  public MailHelper mail () {
+    if (mailHelper == null ) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
   }
 }
 
---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appManager/MailHelper.java
@@ -0,0 +1,61 @@
+package ru.stqa.pft.mantis.appManager;
+
+import com.sun.xml.internal.org.jvnet.mimepull.MIMEMessage;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MailHelper {
+
+  private ApplicationManager app;
+  private final Wiser wiser;
+
+
+  public MailHelper(ApplicationManager app) {
+    this.app = app;
+    wiser = new Wiser();
+  }
+
+  public List<MailMessage> waitForMail (int count, long timeout) throws MessagingException, IOException {
+    long start = System.currentTimeMillis();
+    while (System.currentTimeMillis() < start + timeout) {
+      if (wiser.getMessages().size() >= count) {
+
+        return wiser.getMessages().stream().map((g) -> toModelMail(g)).collect(Collectors.toList());
+      }
+      try {
+        Thread.sleep (1000);
+        }
+        catch (InterruptedException e) {
+        e.printStackTrace();
+
+      }
+    }
+    throw new Error("No email :(");
+  }
+
+  private static MailMessage toModelMail(WiserMessage m) throws IOException {
+    try {
+      MimeMessage mm = m.getMimeMessage ();
+      return new MailMessage (mm.getAllRecipients()[0].toString(), (String) mm.getContent());
+      }
+      catch (MessagingException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public void start () {
+    wiser.start();
+  }
+
+  public void stop () {
+    wiser.stop();
+  }
+}
