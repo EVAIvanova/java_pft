b68b3fe3241c74283e3e4de942136ba39bd46326
EVAIvanova <skyLena1@ya.ru>
2017-12-02 14:46:27 +0200

    тест RegistrationTest() выбрасывает исключение в строках telnet.connect() и store.connect()

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appManager/JamesHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appManager/JamesHelper.java
@@ -2,11 +2,17 @@ package ru.stqa.pft.mantis.appManager;
 
 import com.sun.imageio.plugins.common.ReaderUtil;
 import org.apache.commons.net.telnet.TelnetClient;
+import org.subethamail.wiser.WiserMessage;
+import ru.stqa.pft.mantis.model.MailMessage;
 
-import javax.mail.Session;
-import javax.mail.Store;
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JamesHelper {
 
@@ -19,7 +25,7 @@ public class JamesHelper {
   private Store store;
   private String mailserver;
 
-  public JamesHelper () {
+  public JamesHelper (ApplicationManager app) {
     this.app = app;
     telnet = new TelnetClient();
     mailSession = Session.getDefaultInstance(System.getProperties());
@@ -33,17 +39,158 @@ public class JamesHelper {
     return result.trim().equals("User" + name + " exist");
   }
 
+    private void initTelnetSession() {
+
+    mailserver = app.getProperty("mailserver.host");
+    int port = Integer.parseInt(app.getProperty("mailserver.port"));
+    String login = app.getProperty("mailserver.adminlogin");
+    String password = app.getProperty("mailserver.adminpassword");
+
+    try {
+      telnet.connect(mailserver, port);
+      in = telnet.getInputStream();
+      out = new PrintStream(telnet.getOutputStream());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    readUntil("Login id:");
+    write("");
+    readUntil("Password: ");
+    write("");
+
+    readUntil("Login id:");
+    write(login);
+    readUntil("Password: ");
+    write(password);
+
+    readUntil("Welcome " + login +" HELP for a list of commands");
+  }
+
+  public void createUser (String name, String passwd) {
+    initTelnetSession();
+    write ("adduser " + name + " " + passwd);
+    String result = readUntil ("user " + name + "added");
+    closeTelnetSession();
+  }
+
+  public void deleteUser (String name, String passwd) {
+    initTelnetSession();
+    write ("deluser " + name );
+    String result = readUntil ("user " + name + "deleted");
+    closeTelnetSession();
+  }
+
+  private String readUntil(String pattern) {
+
+    try {
+      char lastChar = pattern.charAt(pattern.length()-1);
+      StringBuffer sb = new StringBuffer();
+      char ch = (char) in.read();
+      while (true) {
+        System.out.print(ch);
+        sb.append(ch);
+        if (ch == lastChar) {
+          if ( sb.toString().endsWith(pattern)) {
+            return sb.toString();
+          }
+        }
+        ch = (char) in.read();
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  private void write (String value) {
+    try {
+      out.println(value);
+      out.flush();
+      System.out.println(value);
+      }
+      catch (Exception e) {
+      e.printStackTrace();
+      }
+  }
+
   private void closeTelnetSession() {
+  write ("quit");
   }
 
-  private String readUntil(String exist) {
+  public void drainEmail (String user, String password) throws MessagingException {
+    Folder inbox = openInbox (user,password);
+    for (Message message : inbox.getMessages()) {
+      message.setFlag(Flags.Flag.DELETED, true);
+      }
+      closeFolder (inbox);
   }
 
-  private void initTelnetSession() {
+  private Folder openInbox(String user, String password) throws MessagingException {
+    store = mailSession.getStore("pop3");
+    try {
+    store.connect(mailserver,user,password);
+    Folder folder = store.getDefaultFolder().getFolder("INBOX");
+    folder.open(Folder.READ_WRITE);
+    return folder;
+  } catch (Exception e) {
+      e.printStackTrace();
+    return null;
+    }
+    }
+
+  private void closeFolder (Folder folder) throws MessagingException {
+    folder.close(true);
+    store.close();
   }
 
-  public void createUser () {
+  public List<MailMessage> waitForMail (String user, String password, long timeout) throws MessagingException, IOException {
+    long now = System.currentTimeMillis();
+    while (System.currentTimeMillis() < now + timeout) {
+      List<MailMessage> allMail = getAllMail (user,password);
+      if (allMail.size() >= 0) {
+        return allMail;
+      }
+      try {
+        Thread.sleep (1000);
+      }
+      catch (InterruptedException e) {
+        e.printStackTrace();
 
+      }
+    }
+    throw new Error("No email :(");
   }
 
+  private List<MailMessage> getAllMail(String user, String password) throws MessagingException {
+    Folder inbox = openInbox(user, password);
+    List<MailMessage> messages = Arrays.asList(inbox.getMessages()).stream().map((m) -> {
+      try {
+        return toModelMail(m);
+      } catch (MessagingException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return null;
+    })
+            .collect(Collectors.toList());
+    closeFolder(inbox);
+    return messages;
+  }
+
+  private static MailMessage toModelMail(Message m) throws MessagingException, IOException {
+    try {
+        return new MailMessage (m.getAllRecipients()[0].toString(), (String) m.getContent());
+    }
+    catch (MessagingException e) {
+      e.printStackTrace();
+      return null;
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.mantis.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -12,25 +10,27 @@ import java.util.List;
 
 import static org.testng.AssertJUnit.assertTrue;
 
-public class RegistrationTests extends TestBase{
+public class RegistrationTests extends TestBase {
 
-  @BeforeMethod
-   public void startMailServer () {
-   app.mail().start();
+  //  @BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
   }
 
   @Test
-  public void testRegistration () throws IOException, MessagingException {
+  public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String user =  String.format("user%s",now);
+    String user = String.format("user%s", (long) (now/1000000000));
     String password = "password";
-    String email = String.format("user%s@localhost.localdomain",now);
-    app.registration().start(user,email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    String email = String.format("user%s@localhost.localdomain", (long) (now/1000000000));
+    app.james().createUser(user, password);
+    app.registration().start(user, email);
+//    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
 
     app.registration().finish(confirmationLink, password);
-    assertTrue (app.newSession().login(user,password ));
+    assertTrue(app.newSession().login(user, password));
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
@@ -39,8 +39,8 @@ public class RegistrationTests extends TestBase{
     return regex.getText(mailMessage.text);
   }
 
-  @AfterMethod (alwaysRun = true)
-  public void stopMailServer () {
+  // @AfterMethod (alwaysRun = true)
+  public void stopMailServer() {
 
     app.mail().stop();
   }
